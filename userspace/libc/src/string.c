83d26e37556daa79fcbe9b931530ae99353c8ad5
Ryan Webber <wbbr.ryan@gmail.com>
2025-03-16 20:56:36 -0700

    LD command fixes for lua + bringing back the custom floor implementation

--- userspace/libc/src/string.c
+++ userspace/libc/src/string.c
@@ -2,6 +2,38 @@
 #include "string.h"
 #include "unistd.h"
 
+char *strchr(const char *s, int c)
+{
+    while (*s != '\0')
+    {
+        if (*s == c)
+        {
+            return (char *)s;
+        }
+        s++;
+    }
+
+    return c == '\0' ? (char *)s : NULL;
+}
+
+char *strdup(const char *s1)
+{
+    size_t len = strlen(s1);
+    char *dup = (char *)malloc(len + 1);
+    if (dup == NULL)
+    {
+        return NULL;
+    }
+
+    for (size_t i = 0; i < len; i++)
+    {
+        dup[i] = s1[i];
+    }
+
+    dup[len] = '\0';
+    return dup;
+}
+
 size_t strlen(const char *s)
 {
     size_t len = 0;
@@ -30,20 +62,6 @@ int strncmp(const char *s1, const char *s2, size_t n)
     return 0;
 }
 
-char *strchr(const char *s, int c)
-{
-    while (*s != '\0')
-    {
-        if (*s == c)
-        {
-            return (char *)s;
-        }
-        s++;
-    }
-
-    return c == '\0' ? (char *)s : NULL;
-}
-
 char *strsep(char **stringp, const char *delim)
 {
     char *string = *stringp;
@@ -68,20 +86,71 @@ char *strsep(char **stringp, const char *delim)
     return tok_start;
 }
 
-char *strdup(const char *s1)
+void *memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
-    size_t len = strlen(s1);
-    char *dup = (char *)malloc(len + 1);
-    if (dup == NULL)
+    char *d = dst;
+    const char *s = src;
+    for (size_t i = 0; i < n; i++)
     {
-        return NULL;
+        d[i] = s[i];
     }
+    return dst;
+}
 
+void *memmove(void *dst, const void *src, size_t n)
+{
+    char *d = dst;
+    const char *s = src;
+    if (d < s)
+    {
+        for (size_t i = 0; i < n; i++)
+        {
+            d[i] = s[i];
+        }
+    }
+    else
+    {
+        for (size_t i = n; i > 0; i--)
+        {
+            d[i - 1] = s[i - 1];
+        }
+    }
+    return dst;
+}
+
+void *memset(void *b, int c, size_t len)
+{
+    char *dst = b;
     for (size_t i = 0; i < len; i++)
     {
-        dup[i] = s1[i];
+        dst[i] = c;
     }
+    return b;
+}
 
-    dup[len] = '\0';
-    return dup;
+void *memchr(const void *s, int c, size_t n)
+{
+    const unsigned char *src = s;
+    for (size_t i = 0; i < n; i++)
+    {
+        if (src[i] == c)
+        {
+            return (void *)&src[i];
+        }
+    }
+    return NULL;
+}
+
+int memcmp(const void *s1, const void *s2, size_t n)
+{
+    const unsigned char *src1 = s1;
+    const unsigned char *src2 = s2;
+    for (size_t i = 0; i < n; i++)
+    {
+        if (src1[i] != src2[i])
+        {
+            return src1[i] - src2[i];
+        }
+    }
+    return 0;
 }
--- userspace/libm/src/math.c
+++ userspace/libm/src/math.c
@@ -1,107 +1,27 @@
 #include <math.h>
 #include <float.h>
 
-double acos(double x)
+union x87_control_word get_cw_x87()
 {
-    return __builtin_acos(x);
+    union x87_control_word cw;
+    asm("fnstcw %0"
+        : "=m"(cw));
+    return cw;
+}
+void set_cw_x87(union x87_control_word control_word)
+{
+    asm("fldcw %0" ::"m"(control_word));
 }
 
-double asin(double x)
+double floor(double value)
 {
-    return __builtin_asin(x);
-}
+    union x87_control_word cw = get_cw_x87();
+    union x87_control_word new_cw = cw;
+    new_cw.rounding_control = ROUNDING_MODE_DOWN;
+    set_cw_x87(new_cw);
+    asm("frndint"
+        : "+t"(value));
 
-double atan2(double y, double x)
-{
-    return __builtin_atan2(x, y);
-}
-
-double ceil(double x)
-{
-    return __builtin_ceil(x);
-}
-
-double cos(double x)
-{
-    return __builtin_cos(x);
-}
-
-double cosh(double x)
-{
-    return __builtin_cosh(x);
-}
-
-double exp(double x)
-{
-    return __builtin_exp(x);
-}
-
-double fabs(double x)
-{
-    return __builtin_fabs(x);
-}
-
-double floor(double x)
-{
-    return __builtin_floor(x);
-}
-
-double fmod(double x, double y)
-{
-    return __builtin_fmod(x, y);
-}
-
-double frexp(double value, int *exp)
-{
-    return __builtin_frexp(value, exp);
-}
-
-double ldexp(double x, int n)
-{
-    return __builtin_ldexp(x, n);
-}
-
-double log(double x)
-{
-    return __builtin_log(x);
-}
-
-double log10(double x)
-{
-    return __builtin_log10(x);
-}
-
-double log2(double x)
-{
-    return __builtin_log2(x);
-}
-
-double pow(double x, double y)
-{
-    return __builtin_pow(x, y);
-}
-
-double sin(double x)
-{
-    return __builtin_sin(x);
-}
-
-double sinh(double x)
-{
-    return __builtin_sinh(x);
-}
-
-double sqrt(double x)
-{
-    return __builtin_sqrt(x);
-}
-
-double tan(double x)
-{
-    return __builtin_tan(x);
-}
-
-double tanh(double x)
-{
-    return __builtin_tanh(x);
+    set_cw_x87(cw);
+    return value;
 }
